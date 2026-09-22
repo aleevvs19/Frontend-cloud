cbca331c9a7d7c1bc0c747aa81e112137a1d0462
aleevvs19 <ale.salazarv@duocuc.cl>
2026-09-21 22:28:35 -0300

    arreglando errores

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://100.50.157.1/api', // Apunta al puerto 80 de Nginx
+  apiUrl: 'http://54.197.16.20/api', // Apunta a la nueva IP del backend en AWS
   entraScope: 'api://501e8863-bd34-48ca-98ad-2c8ff1f8873d/Ale-Pedidos360',
   apiUris: [
     'http://localhost:8080/*',
-    'http://100.50.157.1/*',       // Cubre Nginx
-    'http://100.50.157.1:8080/*'
+    'http://54.197.16.20/*',
+    'https://54.197.16.20/*'
   ]
 };
